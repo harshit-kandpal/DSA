f48277ce32566dd81112fa1b3ed16191a1b74275
harshit-kandpal <108466485+harshit-kandpal@users.noreply.github.com>
2022-06-30 17:18:05 +0530

    Create factorial.cpp

---
+++ RECURSION/EASY/factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void factorial(int n,int fact){
+  if(n==0){
+    cout<<fact;
+    return;
+  }
+  fact*=n;
+  factorial(n-1;fact);
+  
+}
+
+int main(){
+  int number;
+  cin>>number;
+  
+  factorial(number,1);
+  
+  return 0;
+}
