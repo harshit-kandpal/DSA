403c1632227f3fe26875a4a7cce112467eac9a65
harshit-kandpal <108466485+harshit-kandpal@users.noreply.github.com>
2022-07-01 16:51:24 +0530

    Create sendWordsBack.cpp

---
+++ RECURSION/Intermediate/sendWordsBack.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void sendback(string str,int index,int count,string newstr,char back){
+    if(index == str.size()){
+        while(count--){
+            newstr+=back;
+        }
+        cout<<newstr;
+    }
+    if(str.at(index) == back){
+        count++;
+    }
+    else{
+        newstr+=str.at(index);
+    }
+    
+    sendback(str,index+1,count,newstr,back);
+}
+    
+int main()
+{
+    string str;
+    cin>>str;
+    char back;
+    cin>>back;
+    
+    sendback(str,0,0,"",back);
+    
+    return 0;
+}
