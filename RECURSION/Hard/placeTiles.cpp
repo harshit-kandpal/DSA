9d0ecb6b0f51866e96301d6816ac1bd6f34c0687
harshit-kandpal <108466485+harshit-kandpal@users.noreply.github.com>
2022-07-02 19:52:53 +0530

    Update placeTiles.cpp

--- RECURSION/Hard/placeTiles.cpp
+++ RECURSION/Hard/placeTiles.cpp
@@ -5,12 +5,13 @@ int tiles(int row, int col){
   if(row == 0){
     return 1;
   }
+   if(row<0){
+       return 0;
+   }
+  int way1 = tiles(row-1,col);
+ 
+  int way2 = tiles(row-col,col);
   
-  int way1,way2;
-  way1 = tiles(row-1,col)
-  if(row-col>=0){
-      way2 = tiles(row-col,col);
-  }
   
   return way1+way2;  
 }
